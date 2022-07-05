4fdeccc40d89a96edd6c822e1a3716c9dd8a9e8d
Виталий <absolut83@yandex.ru>
2022-07-05 13:46:51 +0300

    add test in main

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -5,6 +5,7 @@ def index(request):
     HttpResponse.status_code = 'OK'
     return HttpResponse('У меня получилось!')
 
+
 def second_page(request):
     HttpResponse.status_code = 'OK'
     return HttpResponse('А это вторая страница')
