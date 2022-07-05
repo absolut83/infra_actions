aef305d41b3e11f4e90a85baf92a0d3e03025b99
Виталий <absolut83@yandex.ru>
2022-07-05 14:24:19 +0300

    add test in main

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,10 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    HttpResponse(status=200)
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!'), HttpResponse(status=200)
 
 
 def second_page(request):
-    HttpResponse(status=200)
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница'), HttpResponse(status=200)
